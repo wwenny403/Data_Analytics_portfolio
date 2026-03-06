a415e735807799a9389b825e7d94a041c5642633
wwenny403 <wwenny403@gmail.com>
2026-03-05 22:32:11 -0300

    Add initial dataframes for users and orders

--- day1/exercises.py
+++ day1/exercises.py
@@ -1,1 +1,14 @@
-
+## 題目一
+'''
+import pandas as pd
+users = pd.DataFrame({
+    "user_id":[1,2,3],
+    "name":["Alice","Bob","Cindy"]
+})
+orders = pd.DataFrame({
+    "user_id":[1,1,2],
+    "product":["Book","pen","Laptop"]
+})
+print(users)
+print(orders)
+'''
